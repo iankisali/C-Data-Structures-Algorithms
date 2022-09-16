ca95a0bea52a93cf613b9517619573b4031a3dfd
Ian Kisali <iankisali@gmail.com>
2022-09-16 13:00:35 +0300

    added delete functions

--- Data Stuctures/1. Linked List/header.h
+++ Data Stuctures/1. Linked List/header.h
@@ -14,4 +14,9 @@ void printList(struct Node* n);
 void insertHead(struct Node** head_ref, int new_data);
 void insertAfter(struct Node* prev_node, int new_data);
 void insertEnd(struct Node**, int new_data);
+void deleteHead(struct Node** head);
+void deleteList(struct Node** head);
+void deleteEnd(struct Node** head);
+void deleteUser(struct Node** head, int position);
+
 #endif 
