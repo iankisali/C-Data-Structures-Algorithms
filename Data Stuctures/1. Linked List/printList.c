cd4366d7b0bd286b58b6448d39f619a10a78045b
Ian Kisali <iankisali@gmail.com>
2022-09-17 15:08:28 +0300

    added comments for easy understanding

--- Data Stuctures/1. Linked List/printList.c
+++ Data Stuctures/1. Linked List/printList.c
@@ -1,10 +1,14 @@
 #include "header.h"
 
+/*Function to print Linked List*/
 void printList(struct Node* n)
 {
+  //as long as list is not NULL
   while (n != NULL)
   {
+    //print the data of the node
     printf(" %d ", n->data);
+    //update to the next data in node
     n = n->next;
   }
 }
