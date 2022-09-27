b2f6778a7e79ad13b74cd9124505d5a8767468b8
Ian Kisali <iankisali@gmail.com>
2022-09-27 21:38:30 +0300

    header file with print, insert and delete functions

---
+++ Data Stuctures/2. Doubly Linked List/header.h
@@ -0,0 +1,23 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    struct Node* prev;
+} ;
+
+void printList(struct Node* temp);
+void printReverseRec(struct Node* temp);
+void insertHead(struct Node** head, int new_data);
+void insertAfter(struct Node* prevNode, int new_data);
+void insertEnd(struct Node** head, int newData);
+void deleteHead(struct Node** head);
+void deleteUser(struct Node** head, int position);
+void deleteEnd(struct Node** head);
+
+#endif 
