19c584e0d9a98a15adcdadd82f2d6540d55e8302
Ian Kisali <iankisali@gmail.com>
2022-10-14 10:16:32 +0300

    infix to prefix using array

--- Data Stuctures/4. Stack/InfToPreArray.c
+++ Data Stuctures/4. Stack/InfToPreArray.c
@@ -32,7 +32,7 @@ int peek()
 //inserting value and increasint top
 void push(char item)
 {
-    if (isFull)
+    if (isFull())
         return;
     top++;
     stack[top] = item;
@@ -70,27 +70,105 @@ int precedence(char ch)
     return -1;
 }
 
+/*reversing prefix*/
 void reverse(char *expression)
 {
+    int size = strlen(expression);
+    int j = size, i = 0;
+    char temp[size];
 
+    temp[j--] = '\0';
+    while(expression[i] != '\0')
+    {
+        temp[j] = expression[i];
+        j--;
+        i++;
+    }
+    strcpy(expression, temp);
 }
 
+/*reversing brackets*/
 void brackets(char* expression)
 {
-
+    int i = 0;
+    while(expression[i] != '\0')
+    {
+        if (expression[i] == '(')
+            expression[i] = ')';
+        
+        else if (expression[i] == ')')
+            expression[i] = '(';
+        i++;
+    }
 }
 
+/*driver function*/
 int getPost(char* expression)
 {
+    int i, j;
 
+    for (i = 0, j = -1; expression[i]; i++)
+    {
+        //check if operand and add to output
+        if (operand(expression[i]))
+            expression[++j] = expression[i];
+
+        else if(expression[i] == '(')
+            push(expression[i]);
+
+        //if ) pop &print from stack till (
+        else if (expression[i] == ')')
+        {
+            while (!isEmpty(stack) && peek(stack) != '(')
+                expression[++j] = pop(stack);
+
+            if (!isEmpty(stack) && peek(stack) != '(')
+                return -1;
+
+            else
+                pop(stack);
+        }
+        else //operator
+        {
+            while (!isEmpty(stack) && precedence(expression[i]) <= precedence(peek(stack)))
+                expression[++j] = pop(stack);
+
+            push(expression[i]);
+        }
+    }
+    while (!isEmpty(stack))
+        expression[++j] = pop(stack);
+
+    expression[++j] = '\0';
 }
 
 void infixToPrefix(char *expression)
 {
 
+    int size = strlen(expression);
+    //reverse
+    reverse(expression);
+
+    //change brackets
+    brackets(expression);
+
+    //get postfix
+    getPost(expression);
+
+    //reverse 
+    reverse(expression);
 }
 
 int main()
 {
+    printf("Infix is :");
+    
+    char expression[] = "a+b*(c+d)";
+    printf("%s\n", expression);
+    infixToPrefix(expression);
 
+    printf("Prefix is : ");
+    printf("%s", expression);
+
+    return 0;
 }
